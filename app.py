27dded8ce1f0ec78242bd305eef7313f1e2d1b36
Olga Dukic <145249497+OlgaDukic@users.noreply.github.com>
2025-06-11 21:12:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ def prijava():
     return render_template('hvala.html', ime=ime)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  import os
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+
